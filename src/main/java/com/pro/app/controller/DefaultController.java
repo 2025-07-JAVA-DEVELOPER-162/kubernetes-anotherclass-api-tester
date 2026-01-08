5b2c09440673d7e529556093c708d2b34b6300b4
academyitwill <academyitwill@gmail.com>
2026-01-08 12:15:30 +0900

    ..

--- src/main/java/com/pro/app/controller/DefaultController.java
+++ src/main/java/com/pro/app/controller/DefaultController.java
@@ -49,7 +49,7 @@ public class DefaultController {
         //return "안녕 Kubernetes Another Class  ["+ applicationVersion +"]\n";
         //return "Bye Kubernetes Another Class  ["+ applicationVersion +"]\n";
         //return "Hello Kubernetes Another Class ["+ applicationVersion +"]\n";
-        return "파이프라인 스텝2 Kubernetes Another Class ["+ applicationVersion +"]\n";
+        return "파이프라인 스텝2 변경~~~~~~~~~~~~~ Kubernetes Another Class ["+ applicationVersion +"]\n";
     }
     @GetMapping(value ="**")
     public String noPath(){
